e404ff9f029dc9e4939d32dd0056288a86cf08a0
lu-perez <lu-perez@teco.com.ar>
2023-12-13 00:48:36 -0300

    infinite scroll

--- src/pages/List-OompaLoompas.tsx
+++ src/pages/List-OompaLoompas.tsx
@@ -14,32 +14,45 @@ const ListOompaLoompas = () => {
   } = useAppSelector(selectOompaLoompas);
 
   const isInitialRender = useRef(true);
+  const bottomRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     if (isInitialRender.current) {
       dispatch(getOompaLoompas());
       isInitialRender.current = false;
     }
-  }, [dispatch]);
+    
+    const handleScroll = () => {
+      if (
+        bottomRef.current &&
+        window.innerHeight + window.scrollY >= bottomRef.current.offsetTop
+      ) {
+        if (currentPage < totalPages && !isLoading) {
+          dispatch(getOompaLoompas(currentPage + 1));
+        }
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [dispatch, currentPage, totalPages, isLoading]);
 
   return (
-    <main>
-      <h1>Oompa Loompas</h1>
-
-      <ul>
-        {oompaLoompas.map((oompaLoompa) => (
-          <li key={oompaLoompa.id}>{oompaLoompa.first_name}</li>
-        ))}
-      </ul>
-
-      <button
-        type="button"
-        disabled={isLoading || totalPages === currentPage}
-        onClick={() => dispatch(getOompaLoompas(currentPage + 1))}
-      >
-        Next
-      </button>
-    </main>
+    <>
+      <main>
+        <h1>Oompa Loompas List</h1>
+        <ul>
+          {oompaLoompas.map((oompaLoompa) => (
+            <li style={{margin: '30px'}} key={oompaLoompa.id}>{oompaLoompa.first_name}</li>
+          ))}
+        </ul>
+      </main>
+      <div ref={bottomRef}></div>
+      {isLoading && <p>Loading...</p>}
+    </>
   );
 };
 
